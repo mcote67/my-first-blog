e315ae81ee3c2cf2ece15c97e7159d0f8f7405ef
michel <mcote@msgi.ca>
2015-06-23 08:29:21 -0400

    Almost Done

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 __author__ = 'michel'
 
-from django.conf.urls	import	url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^post/new/', views.post_new),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
 ]
 
 
